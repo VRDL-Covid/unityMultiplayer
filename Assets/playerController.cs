67ee5d5b4ef1f1fec5619a7e54a30805190389b7
Guy Collins <gwc593@gmail.com>
2020-04-14 10:35:32 +0100

    Basic project that contains a player and some additional data to be transferred across the multiplay mechanism.
    Signed-off-by: Guy Collins <gwc593@gmail.com>

---
+++ Assets/playerController.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    public float speed = 1f;
+    public int numClicked = 0;
+    Vector3 moveVector;
+    // Start is called before the first frame update
+    void Start()
+    {
+        moveVector = new Vector3(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        getInput();
+
+        transform.position += moveVector.normalized * speed * Time.deltaTime;
+
+    }
+
+    void getInput()
+    {
+        //forward
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            moveVector += transform.forward;
+        }
+
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            moveVector -= transform.forward;
+        }
+
+
+        //backward
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            moveVector -= transform.forward;
+        }
+
+        if (Input.GetKeyUp(KeyCode.S))
+        {
+            moveVector += transform.forward;
+        }
+
+        //left
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            moveVector -= transform.right;
+        }
+
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            moveVector += transform.right;
+        }
+
+        //right
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            moveVector += transform.right;
+        }
+
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            moveVector -= transform.right;
+        }
+
+
+    }
+
+    private void OnMouseDown()
+    {
+        numClicked++;
+        speed += 1.0f;
+    }
+}
