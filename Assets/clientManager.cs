1420010db50dc94c4e5ebade786c398710fb21f9
Guy Collins <gwc593@gmail.com>
2020-04-20 12:34:18 +0100

    Bare bones recive model in place.

---
+++ Assets/clientManager.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using UnityEngine;
+
+public class clientManager : MonoBehaviour
+{
+    Socket clientSocket;
+
+    byte[] dataBuffer;
+    private void Awake()
+    {
+        clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        dataBuffer = new byte[1024];
+        try
+        {
+            clientSocket.Connect("127.0.0.1", 8002);
+            Debug.Log("connected!");
+        } catch (Exception e)
+        {
+            Debug.Log(e);
+        }
+    }
+    private void Start()
+    {
+     clientSocket.BeginReceive(dataBuffer, 0, dataBuffer.Length, SocketFlags.None, recieveCallback, null);
+    }
+    private void recieveCallback(IAsyncResult ar)
+    {
+        int count = clientSocket.EndReceive(ar);
+        string msgRecieve = Encoding.UTF8.GetString(dataBuffer, 0, dataBuffer.Length);
+        Debug.Log(msgRecieve);
+        Array.Clear(dataBuffer, 0, dataBuffer.Length);
+        clientSocket.BeginReceive(dataBuffer, 0, dataBuffer.Length, SocketFlags.None, recieveCallback, null);
+    }
+
+    private void OnDestroy()
+    {
+        try
+        {
+            clientSocket.Close();
+            Debug.Log("closed!");
+        }
+        catch (Exception e)
+        {
+            Debug.Log(e);
+        }
+    }
+
+
+}
